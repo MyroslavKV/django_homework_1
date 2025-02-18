f83a3045ed2e3b200e3ba5b330525454ce8af7c9
Myroslav Kolesnykov <miroslavkolesnykov@gmail.com>
2025-02-18 20:37:08 +0200

    third commit

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import category_list, category_detail, product_list, product_detail
 
+app_name = 'blog'
+
 urlpatterns = [
-    path("categories/", category_list, name="category_list"),
-    path("categories/<int:category_id>/", category_detail, name="category_detail"),
-    path("categories/<int:category_id>/products/", product_list, name="product_list"),
-    path("products/<int:product_id>/", product_detail, name="product_detail"),
+    path('categories/', category_list, name='category_list'),
+    path('categories/<int:category_id>/', category_detail, name='category_detail'),
+    path('categories/<int:category_id>/products/', product_list, name='product_list'),
+    path('products/<int:product_id>/', product_detail, name='product_detail'),
 ]
