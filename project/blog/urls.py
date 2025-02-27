cff1e4faff93a3a6746e7103a8133eaa0115e9ae
Myroslav Kolesnykov <miroslavkolesnykov@gmail.com>
2025-02-27 08:40:06 +0200

    commit

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import category_list, category_detail, product_list, product_detail
-
+from .views import category_list, product_list
 app_name = 'blog'
 
 urlpatterns = [
-    path('categories/', category_list, name='category_list'),
-    path('categories/<int:category_id>/', category_detail, name='category_detail'),
-    path('categories/<int:category_id>/products/', product_list, name='product_list'),
-    path('products/<int:product_id>/', product_detail, name='product_detail'),
+    path('categories/', category_list, name='categories'), 
+    path('products/', product_list, name='products'), 
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -6,27 +6,6 @@ def category_list(request):
     categories = Category.objects.all()
     return render(request, "categories.html", {"categories": categories})
 
-def category_detail(request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-
-    if request.method == "POST":
-        category.delete()
-        messages.success(request, f'Catagory was deleteed')
-        return redirect("category_list")
-
-    return render(request, "category_detail.html", {"category": category})
-
-def product_list(request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    products = category.products.all()
-    return render(request, "products.html", {"category": category, "products": products})
-
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-
-    if request.method == "POST":  
-        product.delete()
-        messages.success(request, f'product was deleted')
-        return redirect("category_list")
-
-    return render(request, "product_detail.html", {"product": product})
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
