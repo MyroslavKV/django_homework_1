6683afd1e5a2e3cc4b5a73d522521ca4cd833517
Myroslav Kolesnykov <miroslavkolesnykov@gmail.com>
2025-03-11 07:23:43 +0200

    message

--- project/blog/views.py
+++ project/blog/views.py
@@ -4,8 +4,12 @@ from .models import Category, Product
 
 def category_list(request):
     categories = Category.objects.all()
+    if not categories:
+        messages.warning(request, "No categories available.")
     return render(request, "categories.html", {"categories": categories})
 
 def product_list(request):
     products = Product.objects.all()
+    if not products:
+        messages.warning(request, "No products available.")
     return render(request, "products.html", {"products": products})
